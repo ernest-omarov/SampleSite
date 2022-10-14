2f5e1cd32d064eb65327c60cc3e842e2bc4f8b11
Ernest Omarov <erikomaroff@gmail.com>
2022-10-14 21:30:37 +0300

    added first page

--- something.js
+++ something.js
@@ -1,3 +1,5 @@
+const header = document.querySelector('.header');
+
 document.querySelector(".logo").addEventListener('click',function(){
   window.scroll({
     top: 0,
@@ -7,10 +9,13 @@ document.querySelector(".logo").addEventListener('click',function(){
 })
 
 function changingColor() {
-  console.log(window.scrollY)
-  if(window.scrollY == 200){
-    document.getElementsByClassName('.header').style.backgroundColor = "red";
+  if(window.scrollY >= 50){
+    header.style.backgroundColor = "#f0f0f0";
+    header.style.transition = "all .3s";
+  }else if (window.scrollY <= 50) {
+    header.style.backgroundColor = "transparent";
+    header.style.transition = "all .3s";
   }
 }
 
-changingColor()
+window.addEventListener("scroll",changingColor);
