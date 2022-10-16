3bf61096d1a2fee06ff4c49ede5e2c4d78584e74
Ernest Omarov <erikomaroff@gmail.com>
2022-10-16 14:31:33 +0300

    working progress

--- something.js
+++ something.js
@@ -8,14 +8,34 @@ document.querySelector(".logo").addEventListener('click',function(){
   });
 })
 
+let prevScrollY = 0;
+
 function changingColor() {
-  if(window.scrollY >= 50){
-    header.style.backgroundColor = "#f0f0f0";
-    header.style.transition = "all .3s";
-  }else if (window.scrollY <= 50) {
-    header.style.backgroundColor = "transparent";
-    header.style.transition = "all .3s";
+  // if scrolling up - show
+  // if scrolling down and scroll Y past 50px - hide
+  if (window.scrollY < prevScrollY) {
+    header.classList.remove("header-animation");
+    if (window.scrollY <= 50) {
+      header.classList.remove("header-changingColor")
+    } else {
+      header.classList.add("header-changingColor")
+    }
+  } else {
+    if (window.scrollY >= 200) {
+      header.classList.add("header-animation");
+    }
   }
+
+  // if(window.scrollY <= 50){
+  //   header.classList.remove("header-animation");
+  //   header.classList.remove("header-changingColor");
+  // }else if (window.scrollY >= 50 && window.scrollY <= 250) {
+  //   header.classList.add("header-changingColor");
+  // }else if (window.scrollY >= 250) {
+  //   header.classList.add("header-animation");
+  // }
+
+  prevScrollY = window.scrollY;
 }
 
-window.addEventListener("scroll",changingColor);
+window.addEventListener("scroll", changingColor);
